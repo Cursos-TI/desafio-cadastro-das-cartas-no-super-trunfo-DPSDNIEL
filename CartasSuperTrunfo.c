464f8e6896993b7df63f3950789f072d4068a860
DPSDNIEL <dpds6109@hotmail.com>
2025-01-25 21:09:51 +0000

    newproject

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,14 +7,46 @@
 //Teste larissa
 
 int main() {
+    printf("Super Trunfo Paises \n");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+
+    int codcidade;
+    char nome [20];
+    float populacao, area, pib;
+    int turistico;
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
+
+    printf("Qual o nome do Pais? \n");
+    scanf("%s", nome);
+
+    printf("Qual o codigo da cidade? \n");
+    scanf("%d", &codcidade);
+
+    printf("Qual a população total do pais? \n");
+    scanf("%f", &populacao);
+
+    printf("Qual a area total do pais? \n");
+    scanf("%f", &area);
+
+    printf("Qual o pib? \n");
+    scanf("%f", &pib);
+
+    printf("Quantos pontos turisticos? \n");
+    scanf("%d", &turistico);~
+
     // Exibição dos Dados das Cartas:
+
+    printf("\n--- Dados Cadastrados ---\n");
+    printf("País: %s - Código da Cidade: %d\n", nome, codcidade);
+    printf("População: %.2f - Área: %.2f\n", populacao, area);
+    printf("Pontos Turísticos: %d - PIB: %.2f\n", turistico, pib);
+
+    return 0;
+
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
