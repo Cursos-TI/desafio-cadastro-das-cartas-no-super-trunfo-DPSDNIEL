6f22631b373d9277ba873c6fcfacd0c27fd9e120
DPSDNIEL <dpds6109@hotmail.com>
2025-01-25 21:11:43 +0000

    commit01

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -44,9 +44,7 @@ int main() {
     printf("País: %s - Código da Cidade: %d\n", nome, codcidade);
     printf("População: %.2f - Área: %.2f\n", populacao, area);
     printf("Pontos Turísticos: %d - PIB: %.2f\n", turistico, pib);
-
-    return 0;
-
+    
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
